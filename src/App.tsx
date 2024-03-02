9f3ce7e74c5193d163a67eb35290df769162f7b9
idcetim <javirofe96@gmail.com>
2024-03-02 17:56:21 +0100

    Add language provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import enMessages from "./locales/en.json"
+import espMessages from "./locales/esp.json"
+import { useSelector } from "react-redux"
+import { IntlProvider } from "react-intl"
+import "./App.css"
 
 function App() {
+  const language = useSelector((s: any) => s.language)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <IntlProvider locale={language === "en" ? "en" : "esp"} messages={language === "en" ? enMessages : espMessages}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </IntlProvider>
+  )
 }
 
-export default App;
+export default App
