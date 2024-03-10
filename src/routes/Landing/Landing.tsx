a7343330d80319b188d989b05448f2a28aaeeaa7
idcetim <javirofe96@gmail.com>
2024-03-10 08:53:56 +0100

    Add introduction parragraph

--- src/routes/Landing/Landing.tsx
+++ src/routes/Landing/Landing.tsx
@@ -6,6 +6,7 @@ const Landing = () => {
       <header>
         <h1>Full Stack and blockchain Developer</h1>
         <strong>Javier Rodríguez</strong>
+        <p>+3 years experience. I love being full stack developer because it permits me build and deploy applications from scratch and solve all the issues found during the process.  </p>
       </header>
       <WorkExperience />
     </section>
