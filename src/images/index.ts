b3801549c67c9f8e06a4afc31672325988a6fd3f
idcetim <javirofe96@gmail.com>
2024-03-10 09:41:16 +0100

    Add techStack images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,9 +1,25 @@
 import downArray from "./downArrow.svg"
 import linkedin from "./linkedin.svg"
 import github from "./github.svg"
+import gitlab from "./gitlab.svg"
+import react from "./react.svg"
+import node from "./node.svg"
+import postgres from "./postgres.svg"
+import solidity from "./solidity.svg"
+import typescript from "./typescript.svg"
+import css from "./css.svg"
+import azure from "./azure.svg"
 
 export const imgs = {
   downArray,
   linkedin,
   github,
+  gitlab,
+  react,
+  node,
+  postgres,
+  solidity,
+  typescript,
+  css,
+  azure,
 }
