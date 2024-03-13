8d73738ceae40b5fbde0d8a7c67daf8202cceb4c
idcetim <javirofe96@gmail.com>
2024-03-13 21:01:07 +0100

    Add bento grid for projects

--- src/routes/Projects/Projects.tsx
+++ src/routes/Projects/Projects.tsx
@@ -10,8 +10,34 @@ const Projects = () => {
   const { title, description, longDescription, stack, github, url } = projectInfo
   return (
     <div className="project-main">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <section className="bento-grid">
+        <div className="left-grid">
+          <div className="container">
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <a href={url} target="blank_" rel="noreferrer">
+              Visit the project
+            </a>
+            <a href={url} target="blank_" rel="noreferrer">
+              Access Github
+            </a>
+          </div>
+        </div>
+        <div className="right-grid">
+          <div className="container">
+            <p>{longDescription.frontend}</p>
+          </div>
+          <div className="container">
+            <p>{longDescription.backend}</p>
+          </div>
+          <div className="container">
+            <p>{longDescription.database}</p>
+          </div>
+          <div className="container">
+            <p>{longDescription.deployment}</p>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
